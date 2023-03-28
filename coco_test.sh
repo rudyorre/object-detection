865169d781b3737a6e777592ca5492cd62238d7f
rudyorre <rudyorre@gmail.com>
2023-03-27 18:09:59 -0700

    Add MMDetection framework

--- coco_test.sh
+++ coco_test.sh
@@ -1,13 +1,20 @@
 mkdir coco_test
 cd coco_test
 
+# COCO 2017 val images
 mkdir images
 cd images
-curl http://images.cocodataset.org/zips/test2017.zip -o test2017.zip
-unzip test2017.zip
-rm test2017.zip
+curl http://images.cocodataset.org/zips/val2017.zip -o val2017.zip
+unzip val2017.zip
+rm val2017.zip
 
+# COCO 2017 val annotations
 cd ../
-curl http://images.cocodataset.org/annotations/image_info_test2017.zip -o image_info_test2017.zip
-unzip image_info_test2017.zip
-rm image_info_test2017.zip
+curl http://images.cocodataset.org/annotations/annotations_trainval2017.zip -o trainval2017.zip
+unzip trainval2017.zip
+rm trainval2017.zip
+
+# YOLO v3 weights
+cd yolo-v3
+curl https://pjreddie.com/media/files/yolov3.weights -o yolov3.weights
+cd ../
